7aa1a6e96dd315a4924b192fc95dda11e30f11af
Kushal-deep <kushaldeep448@gmail.com>
2025-03-19 23:23:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 // complete the given function
 
 function palindrome(str){
-	let string=""
-	for(let i=str.length-1;i>=0;i--){
-		string=string+str[i];
-	}
-	if(str===string){
-		return true;
-	}
-	else{
-		return false
-	}
+// 	let string=""
+// 	for(let i=str.length-1;i>=0;i--){
+// 		string=string+str[i];
+// 	}
+// 	if(str===string){
+// 		return true;
+// 	}
+// 	else{
+// 		return false
+// 	}
 
+// }
+
+
+	 const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+    // Compare characters from the start and end moving towards the center
+    let left = 0;
+    let right = cleanedStr.length - 1;
+
+    while (left < right) {
+        if (cleanedStr[left] !== cleanedStr[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
 }
 module.exports = palindrome
