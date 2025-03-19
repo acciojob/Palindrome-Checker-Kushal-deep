32e263265031c7bc49a43358982338a6a9dc8a31
Kushal-deep <kushaldeep448@gmail.com>
2025-03-19 19:58:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
+	let string=""
+	for(i=str.length-1;i<=0;i--){
+		string=string+str[i];
+	}
+	if(str===string){
+		return true;
+	}
+	else{
+		return false
+	}
 
 }
 module.exports = palindrome
